5dec7f3cf3d9e1bbcf0b682fc7339580f7fc7c94
Jesús López-González <jesus@kathars.es>
2016-09-14 11:49:20 +0200

    Added Parigot's natural numbers

--- src/test/scala/ChurchBasics.scala
+++ src/test/scala/ChurchBasics.scala
@@ -4,9 +4,11 @@ import org.scalatest._
 
 class ChurchBasics extends FlatSpec with Matchers {
 
-  /* Church Encoding */
+  /*************************************************************/
+  /* 1. Non-Recursive datatypes: same Church & Scott Encodings */
+  /*************************************************************/
 
-  // Booleans
+  // 1.1. Booleans
 
   trait Bool {
     def apply[A](t: A, f: A): A
@@ -48,7 +50,114 @@ class ChurchBasics extends FlatSpec with Matchers {
     _if(False) (_then = "hello", _else = "world") shouldBe "world"
   }
 
-  // Naturals
+  // 1.2. Tuples
+
+  trait Tuple[B, C] {
+    def apply[A](both: (B, C) => A): A
+  }
+
+  case class StringIntTuple(s: String, i: Int) extends Tuple[String, Int] {
+    def apply[A](both: (String, Int) => A): A = both(s, i)
+  }
+
+  object Tuple {
+
+    def fst[A, B](t: Tuple[A, B]): A = t((a, _) => a)
+
+    def snd[A, B](t: Tuple[A, B]): B = t((_, b) => b)
+
+    def swap[B, C](t: Tuple[B, C]): Tuple[C, B] = new Tuple[C, B] {
+      def apply[A](both: (C, B) => A): A = t((b, c) => both(c, b))
+    }
+  }
+
+  "Tuple expressions" should "work" in {
+    import Tuple._
+
+    val t = StringIntTuple("text", 0)
+
+    fst(t) shouldBe "text"
+    snd(t) shouldBe 0
+    swap(t) ((i, s) => s"$i -> $s") shouldBe "0 -> text"
+  }
+
+  // 1.3. Temperatures
+
+  trait Temperature {
+    def apply[A](f: Int => A, c: Int => A): A
+  }
+
+  case class Fahrenheit(i: Int) extends Temperature {
+    def apply[A](f: Int => A, c: Int => A): A = f(i)
+  }
+
+  case class Celsius(i: Int) extends Temperature {
+    def apply[A](f: Int => A, c: Int => A): A = c(i)
+  }
+
+  object Temperature {
+    def warm(t: Temperature): Boolean = t[Boolean](_ > 90, _ > 30)
+  }
+
+  "Temperature expressions" should "work" in {
+    import Temperature._
+
+    val t1: Temperature = Fahrenheit(40)
+    val t2: Temperature = Celsius(40)
+
+    warm(t1) shouldBe false
+    warm(t2) shouldBe true
+  }
+
+  /**************************/
+  /* 2. Recursive datatypes */
+  /**************************/
+
+  // 2.1. Naturals
+
+  // 2.1.1. Scott Encoding
+
+  trait SNat {
+    def apply[A](zero: A, succ: SNat => A): A
+  }
+
+  case object SZero extends SNat {
+    def apply[A](zero: A, succ: SNat => A): A = zero
+  }
+
+  case object SOne extends SNat {
+    def apply[A](zero: A, succ: SNat => A): A = succ(SZero)
+  }
+
+  case class SSucc(n: SNat) extends SNat {
+    def apply[A](zero: A, succ: SNat => A): A = succ(n)
+  }
+
+  object SNat {
+
+    def add(n: SNat, m: SNat): SNat = n(m, add(_, m))
+
+    // O(1) complexity
+    def pred(n: SNat): SNat = n(SZero, identity)
+
+    // aka. church to scott...
+    def foldNat[A](zero: A, succ: A => A): SNat => A =
+      _(zero, n => succ(foldNat(zero, succ)(n)))
+
+    def toChurch: SNat => Nat = foldNat(Zero, Succ)
+  }
+
+  "Scott's Nat expressions" should "work" in {
+    import SNat._
+
+    pred(SOne)[Int](0, _ => 1) shouldBe 0
+    pred(SSucc(SOne))[Int](0, _ => 1) shouldBe 1
+
+    add(SZero, SZero)(0, _ => 1) shouldBe 0
+    add(SZero, SOne)(0, _ => 1) shouldBe 1
+  }
+
+  // 2.1.2. Church Encoding
 
   trait Nat {
     def apply[A](zero: A, succ: A => A): A
@@ -78,9 +187,15 @@ class ChurchBasics extends FlatSpec with Matchers {
     def isEven(n: Nat): Bool = n(True, negate)
 
     def isOdd(n: Nat): Bool = negate(isEven(n))
+
+    // It takes `O(n)` to get the predecessor!
+    def pred(n: Nat): Nat =
+      n[(Nat, Nat)]((Zero, Zero), { case (_, x) => (x, Succ(x)) })._1
+
+    def toScott: Nat => SNat = _[SNat](SZero, SSucc)
   }
 
-  "Nat expressions" should "work" in {
+  "Church's Nat expressions" should "work" in {
     import Nat._
 
     isEven(Zero)      (true, false) shouldBe true
@@ -98,9 +213,88 @@ class ChurchBasics extends FlatSpec with Matchers {
     isEven(mul(Zero, Zero))           (true, false) shouldBe true
     isEven(mul(Zero, One))            (true, false) shouldBe true
     isEven(mul(Succ(One), Succ(One))) (true, false) shouldBe true
+
+    pred(Zero)[Int](0, _ + 1)            shouldBe 0 // XXX: weird behaviour!
+    pred(One)[Int](0, _ + 1)             shouldBe 0
+    pred(Succ(One))[Int](0, _ + 1)       shouldBe 1
+    pred(Succ(Succ(One)))[Int](0, _ + 1) shouldBe 2
   }
 
-  // Lists
+  // 2.1.3. Parigot Encoding
+
+  trait PNat {
+    def apply[A](zero: A, succ: (PNat, A) => A): A
+  }
+
+  case object PZero extends PNat {
+    def apply[A](zero: A, succ: (PNat, A) => A): A = zero
+  }
+
+  case object POne extends PNat {
+    def apply[A](zero: A, succ: (PNat, A) => A): A = succ(PZero, zero)
+  }
+
+  case class PSucc(n: PNat) extends PNat {
+    def apply[A](zero: A, succ: (PNat, A) => A): A = succ(n, n(zero, succ))
+  }
+
+  object PNat {
+
+    def add(n: PNat, m: PNat): PNat = n[PNat](m, (p, a) => PSucc(a))
+
+    def pred(n: PNat): PNat = n[PNat](PZero, (p, _) => p)
+  }
+
+  "Parigot's Nat expressions" should "work" in {
+    import PNat._
+
+    pred(POne)[Int](0, (_, i) => i) shouldBe 0
+    pred(PSucc(POne))[Int](0, (_, i) => i + 1) shouldBe 1
+
+    add(PZero, PZero)[Int](0, (_, i) => i) shouldBe 0
+    add(PZero, POne)[Int](0, (_, i) => i + 1) shouldBe 1
+    add(POne, POne)[Int](0, (_, i) => i + 1) shouldBe 2
+  }
+
+  // 2.2. Lists
+
+  // 2.2.1. Scott Encoding
+
+  trait SIntList {
+    def apply[A](nil: => A, cons: (Int, SIntList) => A): A
+  }
+
+  object SNil extends SIntList {
+    def apply[A](nil: => A, cons: (Int, SIntList) => A): A = nil
+  }
+
+  case class SCons(h: Int, t: SIntList) extends SIntList {
+    def apply[A](nil: => A, cons: (Int, SIntList) => A): A = cons(h, t)
+  }
+
+  object SIntList {
+
+    // O(1) complexity
+    def head(l: SIntList): Int =
+      l(throw new Error("Nil.head"), (i, _) => i)
+
+    // O(1) complexity
+    def tail(l: SIntList): SIntList =
+      l(throw new Error("Nil.tail"), (_, t) => t)
+
+    def foldIntList[A](nil: A, cons: (Int, A) => A): SIntList => A =
+      _(nil, (i, xs) => cons(i, foldIntList(nil, cons)(xs)))
+  }
+
+  "Scott's Lists expressions" should "work" in {
+    import SIntList._
+
+    head(SCons(1, SNil)) shouldBe 1
+    head(SCons(2, SCons(1, SNil))) shouldBe 2
+    head(tail(SCons(2, SCons(1, SNil)))) shouldBe 1
+  }
+
+  // 2.2.2. Church Encoding
 
   trait IntList {
     def apply[A](nil: A, cons: (Int, A) => A): A
